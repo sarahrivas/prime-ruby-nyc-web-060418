36256b86d9b3038dbe17b7b7330d57818458a6c7
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-05-27 01:27:28 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,22 +1,13 @@
 def prime?(num)
-  if num < 0
+  i = 2
+  if num <= 0
     false
   end
-  while num > 2
+  
+  while i < num
+  
+end    
     
   
-  prime_nums = []
-  if num == 2 
-    prime_nums.push(num)
-  end
   
-  while num > 2
-      num % i == 0 
-      false 
-      num += 1 
-    end
-      true
-      prime_nums.push(num)
-      num += 1
-  end  
-end    
+   
