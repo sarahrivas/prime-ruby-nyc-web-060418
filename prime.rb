ae4410fc1a83b497b4ddc8e499e4b7a2905d93bd
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-05-27 00:51:41 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,10 +2,7 @@ def prime?(array, num)
   prime_nums = []
   loop do
     array.each do |num|
-      if num == 2 
-        true 
-        prime_nums.push(num)
-      elsif num % i == 0 
+      num % i == 0 
       false 
     end
       true
