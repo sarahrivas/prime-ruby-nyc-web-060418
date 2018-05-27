ccb720b76de34562a7e4c887c3d6a15f1e14f945
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-05-27 01:33:39 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,15 +2,14 @@ def prime?(num)
   i = 2
   if num <= 0
     false
-  end
-  
-  while i < num
-    if num % i == 0 
-      false
+  else  
+    while i < num
+      if num % i == 0 
+        false
+      end
+      i += 1
     end
-    i += 1
-  end
-  true
+    true
 end    
     
   
